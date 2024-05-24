24af91cfe0f3f2a6da2ae3682a32c0f053a6b777
Sakshi <sakshipathak3007@gmail.com>
2024-05-24 18:02:50 +0530

    sameSite none added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -61,7 +61,7 @@ export const signin = async (req, res, next) => {
       .cookie('access_token', token, {
         httpOnly: true,
         secure: true,
-        SameSite: 'none',
+        sameSite: 'none',
       })
       .json(rest);
   } catch (error) {
